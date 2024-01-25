ebff11be37717416a5ce449e3ba7fa3e66d4f479
claire.chang <claire.chang@starnic.com.tw>
2024-01-25 16:57:20 +0800

    feat: 增加自訂 loading 效果

---
+++ components/TheLoadingIndicator.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="backdrop position-fixed start-0 top-0 end-0 bottom-0 d-flex flex-column
+		align-items-center justify-content-center" v-if="isLoading">
+		<div class="spinner-border text-primary-80 opacity-75" role="status">
+			<span class="visually-hidden">Loading...</span>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const isLoading = useLoading();
+</script>
